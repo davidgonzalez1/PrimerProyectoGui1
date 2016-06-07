91ab9f9c5aa256684843e4214e43004f91c2779a
jorgedavid kugonzalez <jorgedavid kugonzalez>
2016-06-07 14:38:47 -0600

    Primer Proyecto en java

---
+++ src/pproyectogui/PProyectoGUI.java
@@ -0,0 +1,14 @@
+package pproyectogui;
+
+
+import gui.Bienvenido;
+
+
+public class PProyectoGUI {
+
+    public static void main(String[] args) {
+      Bienvenido windows = new Bienvenido();
+      windows.setVisible(true);
+    }
+    
+}
